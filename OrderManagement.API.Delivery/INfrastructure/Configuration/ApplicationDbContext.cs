4dc30659a4f675d2e7ee0394d6faaaadbae157a9
iman <dev.imansabet@gmail.com>
2024-01-21 19:58:33 +0330

    Create DB

---
+++ OrderManagement.API.Delivery/INfrastructure/Configuration/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagement.API.Delivery.INfrastructure.Entities;
+
+namespace OrderManagement.API.Delivery.INfrastructure.Configuration
+{
+    public class ApplicationDbContext :  DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        { }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
--- OrderManagement.API.Delivery/Program.cs
+++ OrderManagement.API.Delivery/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.API.Delivery.INfrastructure.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
