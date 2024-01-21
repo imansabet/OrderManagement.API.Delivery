eca903797ceecc9c6986fa321bd008017f16b0d4
iman <dev.imansabet@gmail.com>
2024-01-21 20:29:25 +0330

    Add mapper  (Order , OrderResponse)

---
+++ OrderManagement.API.Delivery/INfrastructure/Configuration/Mapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OrderManagement.API.Delivery.DTO.ExternalApiModel.Response;
+using OrderManagement.API.Delivery.INfrastructure.Entities;
+
+namespace OrderManagement.API.Delivery.INfrastructure.Configuration
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<Order,OrderResponse>().ReverseMap();
+        }
+    }
+}
