2cf667759fd180cb7dec6450bf3d4cfddafad383
iman <dev.imansabet@gmail.com>
2024-01-21 20:14:02 +0330

    SeedDataToDB (Order - Customer)

--- OrderManagement.API.Delivery/INfrastructure/Configuration/ApplicationDbContext.cs
+++ OrderManagement.API.Delivery/INfrastructure/Configuration/ApplicationDbContext.cs
@@ -9,6 +9,25 @@ namespace OrderManagement.API.Delivery.INfrastructure.Configuration
         { }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer { Id = Guid.Parse("f7948d66-ae41-4a2b-8a41-a5dea5750243"), Name = "Customer1" },
+                new Customer { Id = Guid.Parse("ab3c1a63-fefa-4934-824b-e4b5201cb84a"), Name = "Customer2" }
+            );
+
+            modelBuilder.Entity<Order>().HasData(
+                new Order { Id = Guid.NewGuid(), Description = "Order1", CustomerId = Guid.Parse("f7948d66-ae41-4a2b-8a41-a5dea5750243") },
+                new Order { Id = Guid.NewGuid(), Description = "Order2", CustomerId = Guid.Parse("f7948d66-ae41-4a2b-8a41-a5dea5750243") },
+                new Order { Id = Guid.NewGuid(), Description = "Order3", CustomerId = Guid.Parse("ab3c1a63-fefa-4934-824b-e4b5201cb84a") }
+            );
+            modelBuilder.Entity<Customer>()
+                .HasMany(c => c.Orders)
+                .WithOne(o => o.Customer)
+                .HasForeignKey(o => o.CustomerId);
+
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
---
+++ OrderManagement.API.Delivery/Migrations/20240121164223_SeedDataToDB.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OrderManagement.API.Delivery.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataToDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("ab3c1a63-fefa-4934-824b-e4b5201cb84a"), "Customer2" },
+                    { new Guid("f7948d66-ae41-4a2b-8a41-a5dea5750243"), "Customer1" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "CustomerId", "Description" },
+                values: new object[,]
+                {
+                    { new Guid("1c72b89b-41f6-495b-8ac2-ca46cd66edd2"), new Guid("f7948d66-ae41-4a2b-8a41-a5dea5750243"), "Order1" },
+                    { new Guid("55525ef6-97c6-4f07-b814-5075ceb63610"), new Guid("f7948d66-ae41-4a2b-8a41-a5dea5750243"), "Order2" },
+                    { new Guid("c6675f9b-dbec-4f51-9d2e-38e717870c4c"), new Guid("ab3c1a63-fefa-4934-824b-e4b5201cb84a"), "Order3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: new Guid("1c72b89b-41f6-495b-8ac2-ca46cd66edd2"));
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: new Guid("55525ef6-97c6-4f07-b814-5075ceb63610"));
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: new Guid("c6675f9b-dbec-4f51-9d2e-38e717870c4c"));
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("ab3c1a63-fefa-4934-824b-e4b5201cb84a"));
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("f7948d66-ae41-4a2b-8a41-a5dea5750243"));
+        }
+    }
+}
