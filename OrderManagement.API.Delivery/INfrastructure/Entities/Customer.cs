32f39eb705ce3b695e618597a0a495c4e71d7d17
iman <dev.imansabet@gmail.com>
2024-01-21 19:51:24 +0330

    Create Entities (Customer  And Order)

---
+++ OrderManagement.API.Delivery/INfrastructure/Entities/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderManagement.API.Delivery.INfrastructure.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        // Navigation Property
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ OrderManagement.API.Delivery/INfrastructure/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderManagement.API.Delivery.INfrastructure.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+
+        // Foreign Key
+        public Guid CustomerId { get; set; }
+
+        // Navigation Property
+        public Customer Customer { get; set; }
+    }
+}
