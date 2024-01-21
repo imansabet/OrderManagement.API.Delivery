0cf33324c386f70ae177fefa8a2032b8d074cc6c
iman <dev.imansabet@gmail.com>
2024-01-21 20:22:30 +0330

    Create External OrderResponse (DTO)

---
+++ OrderManagement.API.Delivery/DTO/ExternalApiModel/Response/OrderResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderManagement.API.Delivery.DTO.ExternalApiModel.Response
+{
+    public class OrderResponse
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+}
