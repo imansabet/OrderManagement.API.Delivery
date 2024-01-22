bb7eba655d74cd93663d92d855926e66236ecb32
iman <dev.imansabet@gmail.com>
2024-01-22 21:51:11 +0330

    Add more Methods in Interface Create OrderRequest

---
+++ OrderManagement.API.Delivery/DTO/InternalAPIModel/Request/OrderRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderManagement.API.Delivery.DTO.InternalAPIModel.Request
+{
+    public class OrderRequest
+    {
+        public string Description { get; set; }
+        public decimal TotalAmount { get; set; }
+        public Guid CustomerId { get; set; }
+    }
+}
--- OrderManagement.API.Delivery/INfrastructure/Repository/IOrderManagementRepository.cs
+++ OrderManagement.API.Delivery/INfrastructure/Repository/IOrderManagementRepository.cs
@@ -6,6 +6,13 @@ namespace OrderManagement.API.Delivery.INfrastructure.Repository
     public interface IOrderManagementRepository 
     {
         Task<OrderResponse> GetOrderByCustomerIdAsync(CustomerIdRequest customerIdRequest);
+        Task<IEnumerable<OrderResponse>> GetAllOrdersAsync(); 
+
+        Task<OrderResponse> AddOrderAsync(OrderRequest orderRequest); 
+
+        Task<OrderResponse> UpdateOrderAsync(OrderRequest orderRequest);  
+
+        Task<bool> DeleteOrderAsync(Guid orderId);    
 
     }
 }
