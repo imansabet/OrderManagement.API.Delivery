f5c68c6e0fe9d02bc01eb175191a62c0a12bd146
iman <dev.imansabet@gmail.com>
2024-01-23 17:49:17 +0330

    Create OrderResponse External and map to Internal

--- OrderManagement.API.Delivery/INfrastructure/Configuration/Mapper.cs
+++ OrderManagement.API.Delivery/INfrastructure/Configuration/Mapper.cs
@@ -9,6 +9,7 @@ namespace OrderManagement.API.Delivery.INfrastructure.Configuration
         {
             //CreateMap<Order,DTO.ExternalApiModel.Response.OrderResponse>().ReverseMap();
             CreateMap<Order,DTO.InternalAPIModel.Response.OrderResponse>().ReverseMap();
+            CreateMap<DTO.InternalAPIModel.Response.OrderResponse,DTO.ExternalAPIModel.Response.OrderResponse>().ReverseMap();    
             //CreateMap<Customer, DTO.ExternalApiModel.Request.CustomerIdRequest>.ReverseMap();
             //CreateMap<Customer, DTO.InternalAPIModel.Request.CustomerIdRequest>.ReverseMap();
         }
